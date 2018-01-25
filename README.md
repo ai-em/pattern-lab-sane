f780573bbaf7fca3133f7a25772195409dac16f7
Mike Whiting <mail@mikejw.co.uk>
2018-01-25 11:56:00 +0000

    add files

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+pattern-lab-sane
+===
+
+Install-and-run pattern lab for PHP. (To help me and maybe others 'grok' the way everything works.
+
+
---
+++ scripts/post_sane.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# create/fix console
+cp -r ./vendor/pattern-lab/edition-thin/core/console ./vendor/bin/pl-console
+sed -i .bak 's/$baseDir = __DIR__."\/..\/";/$baseDir = __DIR__."\/..\/..\/";/' ./vendor/bin/pl-console
+
+# create server (routing)
+mkdir -p core
+cp -r ./vendor/pattern-lab/edition-thin/core/server ./core/
+
+# create config
+mkdir -p config
+echo "{\"patternengines\":[\"\\\\PatternLab\\\\PatternEngine\\\\Mustache\\\\PatternEngineRule\"]}" > config/patternengines.json
+echo "patternExtension: \"mustache\"" >> config/config.yml
+echo "styleguideKit: \"pattern-lab\/styleguidekit-mustache-default\"" >> config/config.yml
+echo "styleguideKitPath: \"vendor\/pattern-lab\/styleguidekit-mustache-default\"" >> config/config.yml
+
+# install starter kit
+php ./vendor/bin/pl-console --starterkit --install pattern-lab/starterkit-mustache-materialdesign
+
+# perform first generate
+php ./vendor/bin/pl-console --generate
+
+# done
+echo ""
+echo "Run server with \"php ./vendor/bin/pl-console --server\""
+echo ""
+
+
+
+
+
+
+
+
+
+
+
