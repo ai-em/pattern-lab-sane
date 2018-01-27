bbfc7278b6b6e6823d6741162cc8772d982a9bbb
Mike Whiting <mail@mikejw.co.uk>
2018-01-27 16:51:55 +0000

    attempting to fix plugin

--- scripts/post_sane.sh
+++ scripts/post_sane.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export CWD=`pwd`
+
 # create/fix console
 cp -r ./core/console ./vendor/bin/pl-console
 sed -i .bak 's/$baseDir = __DIR__."\/..\/";/$baseDir = __DIR__."\/..\/..\/";/' ./vendor/bin/pl-console
@@ -17,14 +19,24 @@ echo "styleguideKitPath: \"vendor/pattern-lab/styleguidekit-mustache-default\"" 
 # install starter kit
 php ./vendor/bin/pl-console --starterkit --install pattern-lab/starterkit-mustache-demo
 
-# perform first generate
-php ./vendor/bin/pl-console --generate
-
 # install assets
 cd ./vendor/pattern-lab/styleguidekit-assets-default
 npm install
 bower install
 gulp --copy-dist=.././../../public
+cd ${CWD}
+
+# add custom javascript (to enable plugin)
+cat ./vendor/pattern-lab/plugin-reload/dist/js/plugin-reload.js >> ./source/js/custom.js
+cat util/jsLoader.js >> source/js/custom.js
+cat util/enableReloadPlugin.js >> source/js/custom.js
+awk '/<\/body>/{print "  <script type=\"text\/javascript\" src=\"\/js\/custom.js\"></script>"}1' \
+    source/_meta/_01-foot.mustache \
+    > tmp && mv tmp \
+    source/_meta/_01-foot.mustache
+
+# perform first generate
+php ./vendor/bin/pl-console --generate
 
 # done
 echo ""
