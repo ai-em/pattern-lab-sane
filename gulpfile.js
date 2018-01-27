0565571c4d5e255e08d2caf9b84535c3353f15ae
Mike Whiting <mail@mikejw.co.uk>
2018-01-27 09:28:11 +0000

    default build pipeline

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+'use strict';
+ 
+var gulp            = require('gulp');
+var del             = require('del');
+var postcss         = require('gulp-postcss');
+var autoprefixer    = require('autoprefixer');
+var cssnano         = require('cssnano');
+var sourcemaps      = require('gulp-sourcemaps');
+var rename          = require('gulp-rename');
+var stylelint       = require('stylelint');
+var reporter        = require('postcss-reporter');
+var rucksack        = require('rucksack-css');
+var exec            = require('child_process').exec;
+var comment         = require('postcss-comment');
+var variables       = require('postcss-css-variables');
+var postcssimport   = require('postcss-import');
+var nested          = require('postcss-nested');
+var cssnext         = require('postcss-cssnext');
+
+
+var stylerules = {
+  "color-no-invalid-hex": true,
+  "declaration-colon-space-before": "never",
+  "indentation": 2,
+  "number-leading-zero": "always"
+};
+
+var renameFunction = function (path) {
+  path.extname = ".min.css";
+  return path;
+};
+
+var sourceMapLocation = ['dest/css/*.css', '!dest/css/*.min.css'];
+
+gulp.task('generate', function(done) {
+  exec('php ./vendor/bin/pl-console --generate', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    done();
+  });
+});
+
+gulp.task('clean', function () {
+  return del([
+    'public/css/*'
+  ]);
+});
+
+gulp.task('styles', function () {
+
+  // order is important!
+  var plugins = [
+    postcssimport(),
+    autoprefixer({browsers: ['last 5 versions']}),
+    variables(),
+    rucksack(),
+    nested(),
+    //cssnext(),
+    cssnano()
+  ];
+
+  return gulp.src('source/css/style.css')
+    .pipe(postcss(plugins, { parser: comment }))
+    .pipe(gulp.dest('public/css'));
+});
+
+/*
+gulp.task('lint', ['styles'], function() {
+  return gulp.src("public/css/*.css")
+    .pipe(postcss([ stylelint({ "rules": stylerules }), 
+    reporter({ clearMessages: true })
+  ]));
+});
+
+gulp.task('rename', ['lint'], function () {
+  return gulp.src('public/css/*.css')
+    .pipe(rename(renameFunction))
+    .pipe(gulp.dest("public/css/"));
+});
+
+gulp.task('minifyCSS', ['sourcemap'], function () {
+  return gulp.src('public/css/*.min.css')
+    .pipe(nano({ autoprefixer: false }))
+    .pipe(gulp.dest("public/css/"));
+});
+
+gulp.task('sourcemap', ['rename'], function () {
+  return gulp.src(sourceMapLocation)
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('public/css/maps/', {
+      sourceMappingURLPrefix: 'https://www.mydomain.com/'
+    }))
+    .pipe(gulp.dest("public/css/"));
+});
+*/
+
+gulp.task('default', gulp.series('generate', 'clean', 'styles'));
+var watcher = gulp.watch('source/**/*', gulp.series('default'));
